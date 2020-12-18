9b1704fdc35c7864d92390466672f490d7a60bf7
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2020-12-19 00:18:07 +0100

    Bootstrap setup

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,7 +6,12 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+
+import "bootstrap";
+
+import "../channels"
+import "../images";
+import "../styles";
 
 Rails.start()
 Turbolinks.start()
