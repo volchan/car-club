090c726a745e4ffd348d4a9aba6893afd43b95c9
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2020-12-20 21:53:46 +0100

    Pundit setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  include Pundit
+
+  protect_from_forgery with: :exception
+
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
